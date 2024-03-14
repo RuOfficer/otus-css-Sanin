183adc2dbe167c4df54e8f53c7ff11f1c91b9035
KonstantinS <Konstantinruofficer@gmail.ru>
2024-03-14 10:47:07 +1000

    popup изменения

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+let modal = document.getElementById("modal");
+let btn = document.getElementById("openModal");
+let close = document.getElementsByClassName("close")[0];
+let form = document.getElementById("form");
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+close.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let name = document.getElementById("name").ariaValueMax;
+  let email = document.getElementById("email").ariaValueMax;
+});
