c950abeebc2fe644fbfbaf8b76a2fe1eec048927
KonstantinS <Konstantinruofficer@gmail.ru>
2024-03-23 20:45:02 +1000

    Моадльное окно готовое

---
+++ script.js
@@ -0,0 +1,23 @@
+let modal = document.getElementById("modal");
+let btn = document.getElementById("openModal");
+let close = document.getElementsByClassName("btn--color-grey")[0];
+let submit = document.getElementsByClassName("btn--color-black");
+let form = document.getElementById("form");
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+close.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").ariaValueMax;
+  let email = document.getElementById("email").ariaValueMax;
+});
