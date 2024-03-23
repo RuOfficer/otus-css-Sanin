6568cd4a2cdec404511eaff47c42f0f3fc4fd0a1
KonstantinS <Konstantinruofficer@gmail.ru>
2024-03-23 19:27:48 +1000

    Модальное окно

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let modal = document.getElementById("modal");
 let btn = document.getElementById("openModal");
-let close = document.getElementsByClassName("btn--btnClose")[0];
+let close = document.getElementsByClassName("btn--color-grey")[0];
+let submit = document.getElementsByClassName("btn--color-black");
 let form = document.getElementById("form");
 btn.onclick = function () {
   modal.style.display = "block";
 };
 close.onclick = function () {
   modal.style.display = "none";
+};
+submit.onclick = function () {
+  modal.style.display = "block";
 };
 window.onclick = function (event) {
   if (event.target == modal) {
